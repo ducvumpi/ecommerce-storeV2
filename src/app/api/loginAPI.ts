e81195f3c4626e41cff0af1b1eacf8af606d12cf
ducvumpi <ducvumpi3107@gmail.com>
2026-04-10 05:28:05 +0700

    first commit

--- src/app/api/loginAPI.ts
+++ src/app/api/loginAPI.ts
@@ -194,7 +194,7 @@ export async function loginWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: window.location.origin + "/auth/callback",
+      redirectTo: window.location.origin + "/",
     },
   });
 
